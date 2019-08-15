826e6e7d5a25e1f46c07d6fbaa34e9c8e9acdfa9
Beep-cl <rhfjarhfja1@gmail.com>
2019-08-15 10:56:14 +0900

    new file springboot rest api

---
+++ src/main/java/com/rest/api/advice/ExceptionAdvice.java
@@ -0,0 +1,26 @@
+package com.rest.api.advice;
+
+import com.rest.api.advice.exception.CUserNotFoundException;
+import com.rest.api.model.response.CommonResult;
+import com.rest.api.service.ResponseService;
+import lombok.RequiredArgsConstructor;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(CUserNotFoundException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult userNotFoundException(HttpServletRequest request, CUserNotFoundException e) {
+        return responseService.getFailResult();
+    }
+}
